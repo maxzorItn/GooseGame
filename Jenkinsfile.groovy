17a7618d65f3367247c0a0d30b46e8417c7dd3a1
IT545 <it545@infotn.it>
2018-06-01 12:08:11 +0200

    file groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,6 +7,10 @@ node{
     stage('Unit Test'){
         sh "mvn clean test"
         sh "ls -l target"
+        junit 'target/surefire-reports/**/*.xml'
+        def miaVar= "ecco la mia var"
+        echo 'visualizzo $miaVar'
+        echo "visualizzo  $miaVar"
     }
     stage('Integration Test'){
        echo "qui dovrei lanciare i test di Integrazione"
